f4421b7277fedcfccddf8e2a8a60bf18d4152166
Benedict Christian Smit <benedict.smit@tu-dortmund.de>
2025-11-07 23:09:48 +0100

    Add: day-10

--- day-10/src/main.zig
+++ day-10/src/main.zig
@@ -1,73 +1,220 @@
 const std = @import("std");
-const BotQueue = std.SinglyLinkedList;
+const Allocator = std.mem.Allocator;
+const ArrayList = std.ArrayList;
 
+const BotQueue = std.DoublyLinkedList;
+const BotQueueNode = struct {
+    node: BotQueue.Node = .{},
+    val: u32,
+};
 const Destination = union(enum) {
     bot: u32,
     output: u32,
 };
 
-const Instruction = struct {
-    source: u32,
+const GiveInst = struct {
     low_dest: Destination,
     high_dest: Destination,
 };
 
+const Instruction = union(enum) {
+    input: u32,
+    give: GiveInst,
+};
+
+const Line = struct {
+    bot: u32,
+    inst: Instruction,
+};
+
 const input_file = "input.txt";
 pub fn main() !void {
-    const file = try std.fs.cwd().openFile(input_file, .{});
-    const file_size = try file.getEndPos();
-    var page_alloc = std.heap.page_allocator;
-    const input: []u8 = try page_alloc.alloc(u8, file_size);
-    _ = try file.readAll(input);
-    const max_bot_num = try find_highest_bot(input) + 1;
+    const GPA = std.heap.GeneralPurposeAllocator(.{});
+    var dpa = GPA.init;
+    const gpa = GPA.allocator(&dpa);
+    const input = try read_file(gpa, input_file);
+    defer gpa.free(input);
+
+    var lines = try parse_input(gpa, input);
+    defer lines.deinit(gpa);
+
+    const max_bot_num = find_highest_bot(lines.items) + 1;
+
     std.debug.print("{}", .{max_bot_num});
-    var bots = try page_alloc.alloc([2]?u32, max_bot_num);
-    for (0..bots.len) |i| {
+    var bots = try gpa.alloc([2]?u32, max_bot_num);
+    var bot_instrs = try gpa.alloc(?GiveInst, max_bot_num);
+    for (0..max_bot_num) |i| {
         bots[i][0] = null;
         bots[i][1] = null;
+        bot_instrs[i] = null;
     }
-    var bot_instructions = try page_alloc.alloc(?Instruction, max_bot_num);
-    for (0..bot_instructions.len) |i| {
-        bot_instructions[i] = null;
-    }
-    var bot_queue: BotQueue = .{};
-    var lines = std.mem.tokenizeScalar(u8, input, '\n');
-    while (lines.next()) |line| {
-        if (line[0] == 'v') {
-            const value_get = try parse_value_get(line);
-            if (bots[value_get.bot][0] == null) {
-                bots[value_get.bot][0] = value_get.value;
-            } else if (bots[value_get.bot][1] == null) {
-                bots[value_get.bot][1] = value_get.value;
-                const bot = .{ .bot = value_get.bot, .node = .{} };
-                bot_queue.prepend();
-            } else {
-                std.debug.print("Bot {} got more then two value\n ", .{value_get.bot});
-            }
-        } else if (line[0] == 'b') {
-            const instruction = try parse_instruction(line);
-            if (bot_instructions[instruction.source] == null) {
-                bot_instructions[instruction.source] = instruction;
-            } else {
-                std.debug.print("bot {} got more then one instruction\n", .{instruction.source});
-            }
+    var bot_queue = BotQueue{};
+    for (lines.items) |line| {
+        switch (line.inst) {
+            .give => |give| {
+                if (bot_instrs[line.bot] == null) {
+                    bot_instrs[line.bot] = give;
+                } else {
+                    std.debug.print("\n{any}\n", .{bot_instrs[line.bot]});
+                    std.debug.print("\n{any}\n", .{give});
+                    std.debug.print("\nbot {} got more then one give command\n", .{line.bot});
+                    @panic("");
+                }
+            },
+            .input => |x| {
+                if (bots[line.bot][0] == null) {
+                    bots[line.bot][0] = x;
+                } else if (bots[line.bot][1] == null) {
+                    bots[line.bot][1] = x;
+
+                    var new_node = try gpa.create(BotQueueNode);
+                    new_node.* = .{
+                        .val = line.bot,
+                    };
+                    std.debug.print("\ninitial bot {}: {any}\n", .{ line.bot, bots[line.bot] });
+                    bot_queue.append(&new_node.node);
+                } else {
+                    std.debug.print("bot {} got more then 2 vals", .{line.bot});
+                    @panic("");
+                }
+            },
         }
     }
-    while (bot_queue.popFirst) |next_bot| {
-        std.debug.print("next: {}", .{next_bot});
+    std.debug.print("\n", .{});
+    print_queue(bot_queue);
+    var mul: u32 = 1;
+    while (bot_queue.popFirst()) |next_node| {
+        const queuenode: *BotQueueNode = @fieldParentPtr("node", next_node);
+        defer gpa.destroy(queuenode);
+        const next_bot = queuenode.*.val;
+
+        const give_com = bot_instrs[next_bot].?;
+        std.debug.print("\nBot {}: {any}\n", .{ next_bot, bots[next_bot] });
+        std.debug.print("give: {any}\n", .{give_com});
+        const low_val = @min(bots[next_bot][0].?, bots[next_bot][1].?);
+        const high_val = @max(bots[next_bot][0].?, bots[next_bot][1].?);
+        if (low_val == 17 and high_val == 61) {
+            std.debug.print("BOT FOUND {}", .{next_bot});
+        }
+        switch (give_com.high_dest) {
+            .bot => |bot_high| {
+                if (bots[bot_high][0] == null) {
+                    bots[bot_high][0] = high_val;
+                } else if (bots[bot_high][1] == null) {
+                    bots[bot_high][1] = high_val;
+                    std.debug.print("queue hi bot {}: {any}\n", .{ bot_high, bots[bot_high] });
+                    var new_node = try gpa.create(BotQueueNode);
+                    new_node.* = .{
+                        .val = bot_high,
+                    };
+                    bot_queue.append(&new_node.node);
+                } else {
+                    std.debug.print("bot {} got more then 2 vals", .{bot_high});
+                    @panic("");
+                }
+            },
+            .output => |output| {
+                if (output >= 0 and output <= 2) mul *= high_val;
+            },
+        }
+        switch (give_com.low_dest) {
+            .bot => |bot_low| {
+                if (bots[bot_low][0] == null) {
+                    bots[bot_low][0] = low_val;
+                } else if (bots[bot_low][1] == null) {
+                    bots[bot_low][1] = low_val;
+                    std.debug.print("queue lo bot {}: {any}\n", .{ bot_low, bots[bot_low] });
+                    var new_node = try gpa.create(BotQueueNode);
+                    new_node.* = .{
+                        .val = bot_low,
+                    };
+                    bot_queue.append(&new_node.node);
+                } else {
+                    std.debug.print("bot {} got more then 2 vals", .{bot_low});
+                    @panic("");
+                }
+            },
+            .output => |output| {
+                if (output >= 0 and output <= 2) mul *= low_val;
+            },
+        }
+        bots[next_bot][0] = null;
+        bots[next_bot][1] = null;
+
+        std.debug.print("\n", .{});
+        print_queue(bot_queue);
+        std.debug.print("output val: {}", .{mul});
     }
 }
+fn print_queue(queue: BotQueue) void {
+    var cur_node = queue.first;
+    while (cur_node) |node| {
+        const elem: *BotQueueNode = @fieldParentPtr("node", node);
+        std.debug.print("{}, ", .{elem.val});
+        cur_node = node.next;
+    }
+    std.debug.print("\n", .{});
+}
+fn find_highest_bot(lines: []Line) u32 {
+    var highest: u32 = 0;
+    for (lines) |line| {
+        highest = @max(highest, line.bot);
+        switch (line.inst) {
+            .give => |give| {
+                switch (give.low_dest) {
+                    .bot => |bot| {
+                        highest = @max(highest, bot);
+                    },
+                    else => {},
+                }
+                switch (give.high_dest) {
+                    .bot => |bot| {
+                        highest = @max(highest, bot);
+                    },
+                    else => {},
+                }
+            },
+            else => {},
+        }
+    }
+    return highest;
+}
 
-fn parse_value_get(line: []const u8) !struct { value: u32, bot: u32 } {
+pub fn parse_input(allocator: Allocator, file_content: []const u8) !ArrayList(Line) {
+    var lines = std.mem.tokenizeScalar(u8, file_content, '\n');
+    const line_count = count_line_numbers(file_content);
+    var line_insts = try std.ArrayList(Line).initCapacity(allocator, line_count);
+    errdefer line_insts.deinit(allocator);
+    var i: usize = 0;
+    while (lines.next()) |line| : (i += 1) {
+        if (line[0] == 'v') {
+            try line_insts.append(allocator, try parse_value_get(line));
+        } else {
+            try line_insts.append(allocator, try parse_instruction(line));
+        }
+    }
+
+    return line_insts;
+}
+
+fn count_line_numbers(content: []const u8) u32 {
+    var count: u32 = 1;
+    for (content) |c| {
+        if (c == '\n') count += 1;
+    }
+    return count;
+}
+
+fn parse_value_get(line: []const u8) !Line {
     var words = std.mem.tokenizeScalar(u8, line, ' ');
     skip(&words, 1);
     const value = try std.fmt.parseInt(u32, words.next().?, 10);
     skip(&words, 3);
     const bot = try std.fmt.parseInt(u32, words.next().?, 10);
-    return .{ .value = value, .bot = bot };
+    return .{ .bot = bot, .inst = .{ .input = value } };
 }
 
-fn parse_instruction(line: []const u8) !Instruction {
+fn parse_instruction(line: []const u8) !Line {
     var words = std.mem.tokenizeScalar(u8, line, ' ');
     skip(&words, 1);
     const bot = try std.fmt.parseInt(u32, words.next().?, 10);
@@ -93,40 +240,28 @@ fn parse_instruction(line: []const u8) !Instruction {
         high_dest = Destination{ .output = high_dest_num };
     }
 
-    return Instruction{ .source = bot, .low_dest = low_dest, .high_dest = high_dest };
-}
-
-fn find_highest_bot(input: []u8) !u32 {
-    var max: u32 = 0;
-    var lines = std.mem.tokenizeScalar(u8, input, '\n');
-    while (lines.next()) |line| {
-        var words = std.mem.tokenizeScalar(u8, line, ' ');
-        if (line[0] == 'v') {
-            skip(&words, 5);
-            const bot_num: u32 = try std.fmt.parseInt(u32, words.next().?, 10);
-            max = if (max > bot_num) max else bot_num;
-        }
-        if (line[0] == 'b') {
-            skip(&words, 1);
-            const bot_num: u32 = try std.fmt.parseInt(u32, words.next().?, 10);
-            max = if (max > bot_num) max else bot_num;
-            skip(&words, 3);
-            if (std.mem.eql(u8, words.next().?, "bot")) {
-                const bot_num_1: u32 = try std.fmt.parseInt(u32, words.next().?, 10);
-                max = if (max > bot_num_1) max else bot_num_1;
-            }
-            skip(&words, 3);
-            if (std.mem.eql(u8, words.next().?, "bot")) {
-                const bot_num_1: u32 = try std.fmt.parseInt(u32, words.next().?, 10);
-                max = if (max > bot_num_1) max else bot_num_1;
-            }
-        }
-    }
-    return max;
+    return .{
+        .bot = bot,
+        .inst = .{
+            .give = .{
+                .low_dest = low_dest,
+                .high_dest = high_dest,
+            },
+        },
+    };
 }
 
 fn skip(iter: *std.mem.TokenIterator(u8, std.mem.DelimiterType.scalar), n: usize) void {
     for (0..n) |_| {
         _ = iter.next();
     }
+}
+
+pub fn read_file(allocator: Allocator, file_url: []const u8) ![]u8 {
+    const file_desc = try std.fs.cwd().openFile(file_url, .{});
+    defer file_desc.close();
+    const file_size = try file_desc.getEndPos();
+    const file_content: []u8 = try allocator.alloc(u8, file_size);
+    _ = try file_desc.readAll(file_content);
+    return file_content;
 }
